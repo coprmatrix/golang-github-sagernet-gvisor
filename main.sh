0514b85b5ae25ebecb0dd3b2af19a8c561910ef1
u <u>
2025-04-04 14:47:43 +0300

    update

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 file=$PWD/*.spec
 path="$(basename $file)"
 pushd "${outdir:-$PWD}"
-content="$(sed -r 's/(Version:)[^0-9\.]*(.*)/\1  \2/g' $file)"
+content="$(sed -r 's/(Version:)[^0-9\.]*(.*)/\1  \2/g; s/%gocheck//' $file)"
 echo "${content}" > "$path"
 spectool --all --get-files "$path"
 popd
